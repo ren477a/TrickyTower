b0ea7c8ca452888a9ed74ee12bdc58a2a6d433a2
Efren Mercado Jr <mercado.efrenjr@gmail.com>
2017-02-13 21:51:37 +0800

    added player class

--- core/src/com/tipqc/trickytower/GameScreen.java
+++ core/src/com/tipqc/trickytower/GameScreen.java
@@ -3,6 +3,7 @@ package com.tipqc.trickytower;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 /**
  * Created by REN on 2/12/2017.
@@ -11,6 +12,8 @@ import com.badlogic.gdx.graphics.GL20;
 public class GameScreen implements Screen {
 
     private TrickyTowerGame game;
+    private Player player;
+    private SpriteBatch sb;
 
     public GameScreen(TrickyTowerGame game) {
         this.game = game;
@@ -18,13 +21,19 @@ public class GameScreen implements Screen {
 
     @Override
     public void show() {
-
+        player = new Player(100, 700);
+        sb = new SpriteBatch();
     }
 
     @Override
     public void render(float delta) {
         Gdx.gl.glClearColor(Constants.BACKGROUND_COLOR.r, Constants.BACKGROUND_COLOR.g, Constants.BACKGROUND_COLOR.b, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        player.update(delta);
+
+        sb.begin();
+        sb.draw(player.getTexture(), player.getPosition().x, player.getPosition().y);
+        sb.end();
     }
 
     @Override
@@ -49,6 +58,7 @@ public class GameScreen implements Screen {
 
     @Override
     public void dispose() {
-
+        player.dispose();
+        sb.dispose();
     }
 }
--- core/src/com/tipqc/trickytower/Player.java
+++ core/src/com/tipqc/trickytower/Player.java
@@ -1,9 +1,49 @@
 package com.tipqc.trickytower;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+
 /**
  * Created by REN on 2/12/2017.
  */
 
 public class Player {
+    private static final int GRAVITY = -15;
+    private Texture texture;
+    private Vector2 position;
+    private Vector2 velocity;
+
+
+
+    public Player(int x, int y) {
+        texture = new Texture("bird.png");
+        position = new Vector2(x, y);
+        velocity = new Vector2(0, 0);
+    }
+
+    public void update(float delta) {
+        if(position.y > 0)
+            velocity.add(0, GRAVITY);
+        velocity.scl(delta);
+        position.add(0, velocity.y);
+
+        if(position.y < 0)
+            position.y = 0;
+
+        velocity.scl(1/delta);
+
+    }
+
+    public void dispose() {
+        texture.dispose();
+    }
+
+    public Vector2 getPosition() {
+        return position;
+    }
+
+    public Texture getTexture() {
+        return texture;
+    }
 
 }
