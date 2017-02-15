8ed07b2c86336d736f30ed51aa09be314b8af9b2
Efren Mercado Jr <mercado.efrenjr@gmail.com>
2017-02-15 21:12:42 +0800

    modified gravity nd jump velocity

--- core/src/com/tipqc/trickytower/Constants.java
+++ core/src/com/tipqc/trickytower/Constants.java
@@ -15,9 +15,9 @@ public class Constants {
     public static final float WORLD_SIZE = 10.0f;
 
 
-    public static final float GRAVITY = -20;
+    public static final float GRAVITY = -50;
     public static final float PLAYER_MOVEMENT_SPEED = 5.0f;
-    public static final float JUMP_VELOCITY = 400;
+    public static final float JUMP_VELOCITY = 1000;
     public static final float LEFT_BOUNDARY = 25;
     public static final float RIGHT_BOUNDARY = 455;
 
